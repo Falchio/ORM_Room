2f9ed4e60ea549455a06db05cb89903e62546240
Falchio <53504233+Falchio@users.noreply.github.com>
2020-04-28 22:38:43 +0300

    Method for work with Room added

--- app/src/main/java/ru/falchio/myroom/MainActivity.java
+++ app/src/main/java/ru/falchio/myroom/MainActivity.java
@@ -3,12 +3,39 @@ package ru.falchio.myroom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private UserPresenter userPresenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userPresenter = new UserPresenter();
+    }
+
+    public void putUser1(View v){
+        userPresenter.putUser1();
+    }
+
+    public void putUser2(View v){
+        userPresenter.putUser2();
+    }
+
+    public void getUsers(View v){
+        userPresenter.getUsers();
+    }
+
+    public void updateUser(View v){
+        userPresenter.updateUser();
+    }
+
+    public void deleteAllUsers(View v){
+        userPresenter.deleteAllUsers();
+    }
+
+    public void deleteUser(View v){
+        userPresenter.deleteUser();
     }
 }
